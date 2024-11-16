b212e635f3d2f7d9232bebd4a77b6f888a8bdda5
Elias Tuhola <elias.tuhola@gmail.com>
2024-11-16 21:09:16 +0200

    Create index.py
    From pygame examples

---
+++ src/index.py
@@ -0,0 +1,31 @@
+import pygame
+
+
+def main():
+  pygame.init()
+
+  screen = pygame.display.set_mode((1280,720))
+
+  clock = pygame.time.Clock()
+
+  while True:
+      # Process player inputs.
+      for event in pygame.event.get():
+          if event.type == pygame.QUIT:
+              pygame.quit()
+              raise SystemExit
+
+      # Do logical updates here.
+      # ...
+
+      screen.fill("purple")  # Fill the display with a solid color
+
+      # Render the graphics here.
+      # ...
+
+      pygame.display.flip()  # Refresh on-screen display
+      clock.tick(60)         # wait until next frame (at 60 FPS)
+
+
+if __name__ == "__main__":
+  main()
