50de83c63fa9459426f8988b72a487dcb7788789
Elias Tuhola <elias.tuhola@gmail.com>
2024-11-26 22:36:38 +0200

    Added start and coverage report tasks

--- tasks.py
+++ tasks.py
@@ -1,5 +1,12 @@
 from invoke import task
 
+
 @task
 def start(ctx):
     ctx.run("python3 src/index.py", pty=True)
+
+
+@task
+def coverage_report(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+    ctx.run("coverage html", pty=True)
