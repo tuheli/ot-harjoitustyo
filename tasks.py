f11a8391f0fbe5ecc1f86735661172ab60511bd9
Elias Tuhola <elias.tuhola@gmail.com>
2024-11-26 22:41:20 +0200

    Added format task

--- tasks.py
+++ tasks.py
@@ -7,6 +7,11 @@ def start(ctx):
 
 
 @task
+def format(ctx):
+    ctx.run("autopep8 --in-place --recursive src", pty=True)
+
+
+@task
 def coverage_report(ctx):
     ctx.run("coverage run --branch -m pytest src", pty=True)
     ctx.run("coverage html", pty=True)
