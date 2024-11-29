431ed5a112ca284fd0674c07f5ea62ac866838ce
Elias Tuhola <elias.tuhola@gmail.com>
2024-11-29 14:18:05 +0200

    Added main menu

--- src/index.py
+++ src/index.py
@@ -1,8 +1,29 @@
+import pygame
+
+from scripts.constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from scripts.game import Game
+from scripts.menu import Menu
 
 
-# NOTE: Alot of the current code is based on a tutorial
-# I found: https://www.youtube.com/watch?v=2gABYM5M0ww
+class App:
+    def __init__(self) -> None:
+        pygame.init()
+        pygame.display.set_caption('The Impossible Game')
+        screen_size = (SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.screen = pygame.display.set_mode(screen_size)
+        self.menu = Menu(self.screen)
+        self.game = Game(self.screen)
+        self.is_on_menu = True
+
+    def toggle_menu(self):
+        self.is_on_menu = not self.is_on_menu
+
+    def run(self):
+        while True:
+            if self.is_on_menu:
+                self.menu.run(self.toggle_menu)
+            else:
+                self.game.run(self.toggle_menu)
 
 
-Game().run()
+App().run()
