381a29b2f70b94b68ed1e4c64f763164f785a79f
Elias Tuhola <elias.tuhola@gmail.com>
2024-12-11 18:34:13 +0200

    Editor app can be stopped

--- src/index_editor.py
+++ src/index_editor.py
@@ -4,17 +4,21 @@ from scripts.constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from scripts.editor.editor import Editor
 
 
-class App:
+class EditorApp:
     def __init__(self) -> None:
         pygame.init()
         pygame.display.set_caption('The Impossible Game - Editor')
         screen_size = (SCREEN_WIDTH, SCREEN_HEIGHT)
         self.screen = pygame.display.set_mode(screen_size)
-        self.editor = Editor(self.screen)
+        self.editor = Editor(self.screen, self.stop)
+        self.is_running = True
+
+    def stop(self):
+        self.is_running = False
 
     def run(self):
-        while True:
+        while self.is_running:
             self.editor.run()
 
-
-App().run()
+if __name__ == '__main__':
+    EditorApp().run()
