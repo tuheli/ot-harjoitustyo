7715e3213c19838f4dce0f531c6ddb20ffc559c0
Elias Tuhola <elias.tuhola@gmail.com>
2024-12-11 18:34:37 +0200

    Editor can be used from game app

--- src/index_game.py
+++ src/index_game.py
@@ -1,22 +1,26 @@
 import pygame
 
+from index_editor import EditorApp
 from scripts.constants import DEFAULT_TILEMAP_PATH, SCREEN_HEIGHT, SCREEN_WIDTH
+from scripts.editor.editor import Editor
 from scripts.game.game import Game
 from scripts.menu.menu import Menu
 from scripts.utils import get_tilemap
 
 
-class App:
+class GameApp:
     def __init__(self) -> None:
         pygame.init()
         pygame.display.set_caption('The Impossible Game')
         screen_size = (SCREEN_WIDTH, SCREEN_HEIGHT)
         self.screen = pygame.display.set_mode(screen_size)
-        self.menu = Menu(self.screen, self.load_game, self.set_active_tilemap_path)
+        self.menu = Menu(self.screen, self.load_game, self.set_active_tilemap_path, self.load_editor)
         self.game = Game(self.screen, self.load_menu, self.load_game)
+        self.editor = Editor(self.screen, self.load_menu)
         self.active_tilemap_path = DEFAULT_TILEMAP_PATH
         self.game.tilemap = get_tilemap(self, self.active_tilemap_path)
         self.is_on_menu = True
+        self.is_on_editor = False
 
     def set_active_tilemap_path(self, tilemap_path):
         print("selected tilemap path", tilemap_path)
@@ -27,16 +31,25 @@ class App:
         tilemap = get_tilemap(self, self.active_tilemap_path)
         self.game.reset(tilemap) # resets necessary things without recreating Game object
         self.is_on_menu = False
+        self.is_on_editor = False
 
     def load_menu(self):
         self.is_on_menu = True
+        self.is_on_editor = False
+
+    def load_editor(self):
+        self.is_on_editor = True
+        self.is_on_menu = False
 
     def run(self):
         while True:
             if self.is_on_menu:
                 self.menu.run()
+            elif self.is_on_editor:
+                self.editor.run()
             else:
                 self.game.run()
 
 
-App().run()
+if __name__ == '__main__':
+    GameApp().run()
