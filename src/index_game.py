3b27fbe619da2c0ed8f4ff5e7f18edc900072ebd
Elias Tuhola <elias.tuhola@gmail.com>
2024-12-11 13:05:34 +0200

    Passing callbacks to menu and game on constructors instead of run

--- src/index_game.py
+++ src/index_game.py
@@ -5,6 +5,8 @@ from scripts.game.game import Game
 from scripts.menu.menu import Menu
 from scripts.utils import get_tilemap
 
+DEFAULT_TILEMAP_PATH = 'tilemap.json'
+
 
 class App:
     def __init__(self) -> None:
@@ -12,27 +14,19 @@ class App:
         pygame.display.set_caption('The Impossible Game')
         screen_size = (SCREEN_WIDTH, SCREEN_HEIGHT)
         self.screen = pygame.display.set_mode(screen_size)
-        self.menu = Menu(self.screen, self.load_game)
-        self.game = Game(self.screen)
-        self.game.tilemap = get_tilemap(self)
+        self.menu = Menu(self.screen, self.load_game, self.set_active_tilemap_path)
+        self.game = Game(self.screen, self.load_menu, self.load_game)
+        self.active_tilemap_path = DEFAULT_TILEMAP_PATH
+        self.game.tilemap = get_tilemap(self, self.active_tilemap_path)
         self.is_on_menu = True
-        self.active_level = 0
-        self.level_attempts = {
-            0: 0,  # level index, attempt count
-            1: 0,
-            2: 0,
-            3: 0,
-        }
 
-    def on_player_died(self):
-        self.level_attempts[self.active_level] += 1
-        print(
-            f'attempt count for level {self.active_level} is {self.level_attempts.get(self.active_level)}')
-        self.load_game(self.active_level)
-
-    def load_game(self, tilemap_path):
+    def set_active_tilemap_path(self, tilemap_path):
         print("selected tilemap path", tilemap_path)
-        tilemap = get_tilemap(self)
+        self.active_tilemap_path = tilemap_path
+
+    def load_game(self):
+        print('loading game')
+        tilemap = get_tilemap(self, self.active_tilemap_path)
         self.game.on_enter_game(tilemap)
         self.is_on_menu = False
 
@@ -44,7 +38,7 @@ class App:
             if self.is_on_menu:
                 self.menu.run()
             else:
-                self.game.run(self.load_menu, self.on_player_died)
+                self.game.run()
 
 
 App().run()
