7613f662c26886f45f2be7162604b69f0784b2c5
Elias Tuhola <elias.tuhola@gmail.com>
2024-12-11 13:31:45 +0200

    Editor improvements
    - R key to reload active tilemap without saving
    - Showing active tilemap path on screen

--- src/scripts/editor/editor.py
+++ src/scripts/editor/editor.py
@@ -1,7 +1,7 @@
 import sys
 import pygame
 
-from scripts.constants import EDITOR_CAMERA_SPEED, TICK_SPEED, TILE_SIZE, TILEMAP_SAVE_PATH
+from scripts.constants import EDITOR_CAMERA_SPEED, TICK_SPEED, TILE_SIZE, DEFAULT_TILEMAP_PATH
 from scripts.tilemap.editor_tilemap import EditorTilemap
 from scripts.utils import save_tilemap_to_json, screen_to_tilemap_position, load_tilemap_data_from_json
 
@@ -10,11 +10,10 @@ class Editor:
     def __init__(self, screen: pygame.Surface):
         self.screen = screen
         self.clock = pygame.time.Clock()
+        self.active_tilemap_path = DEFAULT_TILEMAP_PATH
+        self.load_active_tilemap_from_file()
 
         self.movement = [False, False, False, False]
-        tilemap = load_tilemap_data_from_json(TILEMAP_SAVE_PATH)
-        self.tilemap = EditorTilemap(
-            self, tilemap_data=tilemap['tilemap'], player_start=tilemap['player_start'])
 
         player_position = (
             self.tilemap.player_start[0] * TILE_SIZE, self.tilemap.player_start[1] * TILE_SIZE)
@@ -64,7 +63,7 @@ class Editor:
 
             print(f"Selected tiles: {self.selected_tiles}")
 
-    def render_keybinds_info(self):
+    def render_info(self):
         font = pygame.font.SysFont(None, 24)
         keybinds = [
             ("W/A/S/D", "Move camera"),
@@ -72,22 +71,48 @@ class Editor:
             ("Right Click (Hold)", "Erase tile"),
             ("Ctrl + S", "Save tilemap"),
             ("T", "Set player start at mouse position"),
+            ("R", "Reload tilemap file without saving"),
             ("Arrow Keys", "Move All Tiles"),
         ]
 
         y_offset = 10
         padding = 10
         text_height = font.get_height()
-        total_height = len(keybinds) * (text_height + padding) + padding
+        item_count = len(keybinds) + 1 # +1 for the active tilemap path
+        total_height = item_count * (text_height + padding) + padding 
 
         background_rect = pygame.Rect(5, 5, 350, total_height)
         pygame.draw.rect(self.screen, (50, 50, 50), background_rect)
 
+        # shows active tilemap path on top
+        font = pygame.font.SysFont(None, 24)
+        text_surface = font.render(f"Editing tilemap: {self.active_tilemap_path}", True, (255, 255, 255))
+        text_rect = text_surface.get_rect(topleft=(10, y_offset))
+
+        padding = 5
+        background_rect = pygame.Rect(
+            text_rect.left - padding,
+            text_rect.top - padding,
+            text_rect.width + 2 * padding,
+            text_rect.height + 2 * padding
+        )
+        pygame.draw.rect(self.screen, (50, 50, 50), background_rect)
+
+        self.screen.blit(text_surface, text_rect)
+        
+        y_offset += 40 # margin after active tilemap path
+
+        # shows keybinds
         for key, description in keybinds:
             text_surface = font.render(
                 f"{key}: {description}", True, (255, 255, 255))
             self.screen.blit(text_surface, (10, y_offset))
             y_offset += text_height + padding
+
+    def load_active_tilemap_from_file(self):
+        tilemap = load_tilemap_data_from_json(self.active_tilemap_path)
+        self.tilemap = EditorTilemap(
+            self, tilemap_data=tilemap['tilemap'], player_start=tilemap['player_start'])
 
     def run(self):
         while True:
@@ -97,7 +122,7 @@ class Editor:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_s and pygame.key.get_mods() & pygame.KMOD_LCTRL:
-                        save_tilemap_to_json(self.tilemap, TILEMAP_SAVE_PATH)
+                        save_tilemap_to_json(self.tilemap, self.active_tilemap_path)
                     elif event.key == pygame.K_w:
                         self.movement[0] = True
                     elif event.key == pygame.K_a:
@@ -116,6 +141,8 @@ class Editor:
                         self.tilemap.move_tiles(0, 1)
                     elif event.key == pygame.K_t:
                         self.set_player_start()
+                    elif event.key == pygame.K_r:
+                        self.load_active_tilemap_from_file()
                 elif event.type == pygame.KEYUP:
                     if event.key == pygame.K_w:
                         self.movement[0] = False
@@ -171,7 +198,7 @@ class Editor:
                 )
                 pygame.draw.rect(self.screen, (255, 255, 0), selection_rect, 2)
 
-            self.render_keybinds_info()
+            self.render_info()
 
             pygame.display.update()
             self.clock.tick(TICK_SPEED)
